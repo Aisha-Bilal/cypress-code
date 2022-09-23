f223069c4e1eb3f202c75a1f156ceaafb17c084c
Aisha bilal <iiisha.12b@gmail.com>
2022-09-23 08:38:01 +0100

    log in cases

--- cypress scripting/cypress/e2e/spec.cy.js
+++ cypress scripting/cypress/e2e/spec.cy.js
@@ -1,5 +1,22 @@
+const { not } = require("check-more-types")
+
 describe('log in page', () => {
-  it('passes', () => {
+ 
+  it('log in with -neg', () => {
+    cy.visit('https://learn.stagingaia.com/login')
+    cy.get(':nth-child(2) > .style_inputDesign__2ZMHE').type("acadifysuperadmin11@gmail.com")
+    cy.get(':nth-child(4) > .style_inputDesign__2ZMHE').type("Password@123")
+    cy.get('.btn').click()
+    cy.url().should.not.be('eq', 'https://learn.stagingaia.com/dashboard')
+  })
+  it('log in with -neg', () => {
+    cy.visit('https://learn.stagingaia.com/login')
+    cy.get(':nth-child(2) > .style_inputDesign__2ZMHE').type("acadifysuperadmin@gmail.com")
+    cy.get(':nth-child(4) > .style_inputDesign__2ZMHE').type("Password@12345")
+    cy.get('.btn').click()
+    cy.url().should.not('eq', 'https://learn.stagingaia.com/dashboard')
+  })
+  it('log in with correct cradentionals', () => {
     cy.visit('https://learn.stagingaia.com/login')
     cy.get(':nth-child(2) > .style_inputDesign__2ZMHE').type("acadifysuperadmin@gmail.com")
     cy.get(':nth-child(4) > .style_inputDesign__2ZMHE').type("Password@123")
